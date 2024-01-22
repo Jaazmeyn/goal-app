3af8617a85c4bf1f4559be351cd76761964b3f07
Jasmin Blanda <jasmin.blanda@gmail.com>
2024-01-22 15:44:39 +0100

    delete goal onPress

--- App.js
+++ App.js
@@ -9,18 +9,34 @@ export default function App() {
   const [liveGoals, setLiveGoals] = useState([]);
 
   function addGoalHandler(enteredGoalText) {
+    //stateupdate
     setLiveGoals( currentLiveGoals => [
       ...currentLiveGoals, 
      { text: enteredGoalText, id: Math.random().toString()},
       ]);
+  }
+  // give function as prop to use it in the component GoalItem.js
+  function deleteGoalHandler(id){
+    setLiveGoals(currentLiveGoals => {
+      return currentLiveGoals.filter((goal) => goal.id !== id); 
+    });
+    
+    console.log('DELETE');
   }
 
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler}/>
       <View style={styles.goalsContainer}>
-        <FlatList data={liveGoals} renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text}/>;
+        <FlatList data={liveGoals} renderItem={(itemData) => { //to be able to scroll performance optimized
+          return (
+            <GoalItem 
+                text={itemData.item.text} 
+                id={itemData.item.id} 
+                onDeleteItem={deleteGoalHandler}
+                
+            />
+          );
         }}
         keyExtractor={(item, index) => { //flatlist key property wir geben id des objekts mit
           return item.id;
