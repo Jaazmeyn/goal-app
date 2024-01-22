d6a2aa7fab5e616763cc991bef7bf2034148d0fe
Jasmin Blanda <jasmin.blanda@gmail.com>
2024-01-22 16:03:01 +0100

    styling onpress feedback ios & android

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props){
     return  (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>        
+            <Pressable 
+                android_ripple={{color: '#dddddd'}} 
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({pressed}) => pressed && styles.pressedItem} //object destructuring otherwise PressData.pressed but used {} provided by Pressable
+                >
                 <Text style={styles.goaltext}>{props.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     )
 }
 
@@ -15,14 +19,18 @@ export default GoalItem;
 const styles = StyleSheet.create({
         goalItem: {
             margin: 8,
-            padding: 8,
             borderRadius: 6,
             borderWidth: 1,
             borderColor: '#cccccc',
-            backgroundColor: '#fef',
+            backgroundColor: '#f2ef',
+          },
+          pressedItem: {
+            opacity: 0.5
           },
           goaltext: {
-            color: 'red',
+            color: 'black',            
+            padding: 8,
+
           }
     }
 )
